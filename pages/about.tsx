f64df654f00ac837ee08ddec1a18bdfea7c40aab
rdnm0 <rohandhanam@gmail.com>
2024-02-24 23:58:25 -0500

    misc

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,7 +2,10 @@
 export default function About() {
     return (
         <div className="min-h-screen bg-black overflow-hidden flex items-center justify-center">
-            <div className="h-screen w-full flex flex-col place-items-center justify-evenly  text-center">
+            <div className="min-h-screen w-full flex flex-col place-items-center justify-evenly  text-center">
+            <h1 className="text-center absolute opacity-20 text-aqua text-[30em] select-none">
+                        SEVA
+                    </h1>	
                 <div className="border-2 border-theme-blue p-8 bg-black/20 shadow-[0_0_60px_10px_#89A8C6] hover:scale-105 transition-transform duration-500 ease-in-out transform">
                     <button className="text-7xl font-bold">Who Are We?</button> 
                 </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,15 @@ import Navbar from "@/components/navbar";
 
 export default function Home() {
    return (
-       <div className=" bg-black">
+       <div className="min-h-screen bg-black">
            <div className="h-screen flex flex-col justify-center place-items-center">
-				
+           <h1 className="text-center absolute opacity-20 text-aqua text-[30em] select-none">
+                        SEVA
+                    </h1>	
         <h1 className="text-center z-10 text-[4em] text-white  ">
 					Uniting Volunteers,<br />Transforming Communities
 				</h1>
+                
            </div>
        </div>     
    );
