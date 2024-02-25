0f6f1f0d9fcdb948894d37f45f525b1cf36bcfff
rdnm0 <rohandhanam@gmail.com>
2024-02-24 20:21:44 -0500

    Test

--- pages/register.tsx
+++ pages/register.tsx
@@ -1,15 +1,1 @@
 import React from "react";
-import { useRouter } from "next/router";
-
-
-function Register () {
-   const router = useRouter();
-
-
-   return (
-       <div>
-           <h1>Register</h1>
-       </div>
-   )
-}
-export default Register;
